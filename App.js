0137f0e32a21281a2665fc2b99a582101c3bf476
Juliana Teixeira <61890719+texjuliana@users.noreply.github.com>
2021-12-27 18:25:12 -0300

    alteração para cadastrar tarefas

--- App.js
+++ App.js
@@ -1,75 +1,124 @@
-import React,{useState} from 'react';
-import {View, Text, StyleSheet, SafeAreaView, StatusBar, TouchableOpacity, FlatList, Modal} from 'react-native';
-import {Ionicons} from '@expo/vector-icons';
+import React, { useCallback, useState } from 'react';
+import { View, Text, StyleSheet, SafeAreaView, StatusBar, TouchableOpacity, FlatList, Modal, TextInput } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 import TaskList from './src/components/TaskList';
 import * as Animatable from 'react-native-animatable';
 
 const AnimatableBtn = Animatable.createAnimatableComponent(TouchableOpacity);
 
-export default function App(){
-  const [task, setTask] = useState([
-    {key: 1, task: 'Comprar pão'},
-    {key: 2, task: 'Estudar React Native'},
-    {key: 3, task: 'Ir na academia hoje a noite'},
-    {key: 4, task: 'Comprar chocolate e coca cola'},
-    {key: 5, task: 'Assitir o 1° video'},
 
-  ]);
+
+
+export default function App() {
+  const [task, setTask] = useState([]);
 
   const [open, setOpen] = useState(false);
+  const [input, setInput] = useState('');
+
+  function handleAdd() {
+    if (input === '') return;
+
+    const data = {
+      key: input,
+      task: input
+    };
+
+    setTask([...task, data]);
+    setOpen(false);
+    setInput('');
+
+  }
+
+
+  const handleDelete = useCallback(
+    (data) => {
+      const find = task.filter(r => r.key !== data.key);
+      setTask(find);
+      
+    }
+  );
+
 
   return (
     <SafeAreaView style={styles.container}>
-      <StatusBar backgroundColor='#171d31' barStyle="ligth-content"/>
+
+      <StatusBar backgroundColor='#171d31' barStyle="ligth-content" />
       <View style={styles.content}>
-     <Text style={styles.title}> Minhas Tarefas </Text>
-     </View>
+        <Text style={styles.title}> Minhas Tarefas </Text>
+      </View>
 
-     <FlatList
-     marginHorizontal={10}
-     showsHorizontalScrollIndicator={false} // desativa barra de scroll
-     data={task} // contem todos itens da lista
-     keyExtractor={(item) => String(item.key)} // cada item tem uma chave
-     renderItem={({item}) => <TaskList data={item} />} // rederiza (mostra) os itens
-     />
+      <FlatList
+        marginHorizontal={10}
+        showsHorizontalScrollIndicator={false} // desativa barra de scroll
+        data={task} // contem todos itens da lista
+        keyExtractor={(item) => String(item.key)} // cada item tem uma chave
+        renderItem={({ item }) => <TaskList data={item} handleDelete={handleDelete} />} // rederiza (mostra) os itens
+      />
 
-     <Modal animationType="slide" transparent={false} visible={open}>
-       <SafeAreaView>
-         <Text> Modal 12312</Text>
-       </SafeAreaView>
-     </Modal>
+      <Modal animationType="slide" transparent={false} visible={open}>
+        <SafeAreaView>
+          <Animatable.View style={styles.modalBody} animation="fadeInUp" useNativeDriver >
 
-     <AnimatableBtn style={styles.fab}
-     style={styles.fab}
-     useNativeDriver
-     animation="bounceInUp"
-     duration={1500}
-     onPress={() => setOpen(true)}
-     >
-       <Ionicons name="ios-add" size={35} color="#FFF"/>
-     </AnimatableBtn>
+            <View style={styles.modalHeader}>
+              <TouchableOpacity onPress={() => setOpen(false)}>
+                <Ionicons size={35} style={{ marginLeft: 5, marginRight: 5 }} name="md-arrow-back"></Ionicons>
+              </TouchableOpacity>
+              <Text style={styles.modalTitle}>Nova tarefa</Text>
+            </View>
+
+            <TextInput
+              multiline={true}
+              placeholderTextColor="#747474"
+              autoCorrect={false}
+              placeholder="O que precisa fazer hoje"
+              style={styles.input}
+              value={input}
+              onChangeText={(text) => setInput(text)}
+
+              placeholder='O que precisa fazer hoje?'
+              style={styles.input}
+            />
+
+            <TouchableOpacity style={styles.handleAdd} onPress={handleAdd}>
+              <Text style={styles.handleAddText}>Cadastrar</Text>
+            </TouchableOpacity>
+
+          </Animatable.View>
+
+        </SafeAreaView>
+      </Modal>
+
+      <AnimatableBtn style={styles.fab}
+        style={styles.fab}
+        useNativeDriver
+        animation="bounceInUp"
+        duration={1500}
+        onPress={() => setOpen(true)}
+      >
+        <Ionicons name="ios-add" size={35} color="#FFF" />
+      </AnimatableBtn>
+
     </SafeAreaView>
-
   );
 }
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
+  container: {
+    flex: 1,
     backgroundColor: '#171d31'
   },
-  title:{
+  title: {
     marginTop: 10,
     paddingBottom: 10,
     fontSize: 25,
-    textAlign:"center",
-    color:'white'
+    textAlign: "center",
+    color: 'white'
   },
-  fab:{
+  fab: {
     position: 'absolute',
     width: 60,
     heigth: 60,
     backgroundColor: '#0094FF',
-    alignItems:'center',
+    alignItems: 'center',
     justifyContent: 'center',
     borderRadius: 30,
     right: 25,
@@ -82,5 +131,44 @@ const styles = StyleSheet.create({
       width: 1,
       height: 3,
     }
+  },
+
+
+  modal: {
+    flex: 1,
+    backgroundColor: '#171d31',
+  },
+  modalHeader: {
+    marginLeft: 10,
+    marginTop: 20,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  modalTitle: {
+    marginLeft: 15,
+    fontSize: 23,
+    color: '#FFF'
+  },
+  modalBody: {
+    backgroundColor: '#AAA',
+    textAlign: 'center'
+  },
+
+
+  handleAdd: {
+    backgroundColor: '#FFF',
+    marginTop: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: 10,
+    marginRight: 10,
+    height: 40,
+    borderRadius: 5
+  },
+  handleAddText: {
+    fontSize: 20,
   }
+
+
+
 })
